cec25e118d15b42c87050c8a5af1a84c0e0a7b5e
Quinn Cox <qrc.inpa@gmail.com>
2019-03-05 05:02:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,15 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
-end
+birthday_kids = {
+    "Timmy" => 9,
+    "Sarah" => 6,
+    "Amanda" => 27
+}
 
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+end
+end
 
